8d1a07a98a974f374ae47dc26982aa44d37951cc
Irving Popovetsky <irving@honeycomb.io>
2024-05-28 20:11:27 -0700

    autopep8
    Signed-off-by: Irving Popovetsky <irving@honeycomb.io>

--- services-implemented-version/meminator-python/src/server.py
+++ services-implemented-version/meminator-python/src/server.py
@@ -9,19 +9,22 @@ from custom_span_processor import CustomSpanProcessor
 # Acquire a tracer
 tracer = trace.get_tracer("meminator-tracer")
 
-IMAGE_MAX_WIDTH_PX=1000
-IMAGE_MAX_HEIGHT_PX=1000
+IMAGE_MAX_WIDTH_PX = 1000
+IMAGE_MAX_HEIGHT_PX = 1000
 
 app = Flask(__name__)
 # Route for health check
+
+
 @app.route('/health')
 def health():
     result = {"message": "I am here", "status_code": 0}
     return jsonify(result)
 
+
 @app.route('/applyPhraseToPicture', methods=['POST', 'GET'])
 def meminate():
-    input = request.json or { "phrase": "I got you"}
+    input = request.json or {"phrase": "I got you"}
     request_span = trace.get_current_span()
 
     phrase = input.get("phrase", "words go here").upper()
@@ -35,42 +38,43 @@ def meminate():
 
     # Check if the file exists
     if not os.path.exists(input_image_path):
-        request_span.add_event("image_not_found", {"input_image_path": input_image_path, "imageUrl": imageUrl})
+        request_span.add_event("image_not_found", {
+                               "input_image_path": input_image_path, "imageUrl": imageUrl})
         return 'downloaded image file not found', 500
 
     # Define the output image path
     output_image_path = generate_random_filename(input_image_path)
 
     command = ['convert',
-            input_image_path,
-            '-resize', f'{IMAGE_MAX_WIDTH_PX}x{IMAGE_MAX_HEIGHT_PX}>',
-            '-gravity', 'North',
-            '-pointsize', '48',
-            '-fill', 'white',
-            '-undercolor', '#00000080',
-            '-font', 'Angkor-Regular',
-            '-annotate', '0', phrase,
-            output_image_path]
+               input_image_path,
+               '-resize', f'{IMAGE_MAX_WIDTH_PX}x{IMAGE_MAX_HEIGHT_PX}>',
+               '-gravity', 'North',
+               '-pointsize', '48',
+               '-fill', 'white',
+               '-undercolor', '#00000080',
+               '-font', 'Angkor-Regular',
+               '-annotate', '0', phrase,
+               output_image_path]
 
-    request_span.add_event("convert_subprocess_start", {"command": " ".join(command)})
+    request_span.add_event("convert_subprocess_start", {
+                           "command": " ".join(command)})
 
     # Run the command, gracefully catch any errors
     try:
         result = subprocess.run(command, capture_output=True, text=True)
         request_span.add_event("convert_subprocess_end",
-            {"command": " ".join(command),
-            "returncode": result.returncode,
-            "stdout": result.stdout,
-            "stderr": result.stderr})
+                               {"command": " ".join(command),
+                                "returncode": result.returncode,
+                                "stdout": result.stdout,
+                                "stderr": result.stderr})
     except Exception as e:
         request_span.add_event("convert_subprocess_failed",
-            {"command": " ".join(command),
-            "returncode": result.returncode,
-            "stderr": result.stderr,
-            "error": e})
+                               {"command": " ".join(command),
+                                "returncode": result.returncode,
+                                "stderr": result.stderr,
+                                "error": e})
         print("An error occurred:", str(e))
         return 'An error occurred generating your image, sorry', 500
-
 
     # Sub-span version
     # with tracer.start_as_current_span("span-name") as subprocess_span:
@@ -87,6 +91,7 @@ def meminate():
         output_image_path,
         mimetype='image/png'
     )
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=10114)
